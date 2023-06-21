a6b3989a054ac1bb0e8fd626dc241cf0b18fab20
durgaprasad189 <79074293+durgaprasad189@users.noreply.github.com>
2023-06-21 17:37:19 +0530

    Update streamlit_app.py
    added some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy Dinner')
+streamlit.header('Break fast menu ')
+streamlit.text('Omega 3 & bule berry oat meal')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
